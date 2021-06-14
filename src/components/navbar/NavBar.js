f9c317286ccefaf454d8124677425cf508165415
MilanP5G <milan_parmar@hotmail.co.uk>
2021-06-14 19:55:06 +0100

    created Main component to house home page elements

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import navLogo from '../../assets/AdaptiveLogo.png';
-import '../../styling/navbar.css'
+import '../../styling/navbar.css';
 
 const NavBar = () => {
 
