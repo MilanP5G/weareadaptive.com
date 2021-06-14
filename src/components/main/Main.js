56b80ee97ff1c72fb9916c875c06b34ccc19c146
MilanP5G <milan_parmar@hotmail.co.uk>
2021-06-14 20:12:05 +0100

    added supporting text for main title

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -7,6 +7,10 @@ const Main = () => {
     <div className='origin-box'>
       <div className='origin-text'>
         <h1>Your solution, delivered</h1>
+        <p>We partner with capital and commodity market participants that value differentiation
+           through technology.</p>
+        <p>Our experienced teams design and deliver complex solutions, using our proven accelerators,
+           creating long-term competitive advantage for you.</p>
       </div>
     </div>
   )
