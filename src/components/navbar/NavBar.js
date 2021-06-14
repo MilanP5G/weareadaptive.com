01d2e19b6d2f49c656752500f9630d0986b1318a
MilanP5G <milan_parmar@hotmail.co.uk>
2021-06-14 19:21:07 +0100

    added logo to navbar and implemented NavLink

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import '../../assets/AdaptiveLogo.png'
+import { NavLink } from 'react-router-dom';
+import navLogo from '../../assets/AdaptiveLogo.png';
 
 const NavBar = () => {
 
   return (
+    <div className='nav-bar'>
+      <div className='nav-logo'>
+        <NavLink className='nav-adaptive-logo' to='/'>
+          <img id='nav-logo' alt='Adaptive Financial Consulting' src={navLogo}>
+      </div>
+    </div>
 
   )
+
 }
+
+export default NavBar;
